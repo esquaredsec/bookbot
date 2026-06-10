025c3d164d8e79da8c1df2487d6055847faf8957
esquaredsec <16594866+esquaredsec@users.noreply.github.com>
2026-06-10 17:51:20 -0400

    adding a charcter counting function and cleaning up some function names for clarity

--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def word_counter(text):
     word_list = text
     words = len(word_list.split())
     return words
+
+def char_counter(text):
+    char_dict = {}
+    char_counts = text.lower()
+    for char in char_counts:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+
+    return char_dict
